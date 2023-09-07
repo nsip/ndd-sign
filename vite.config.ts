f493129109374d12db40a8cc5738360ea604d499
Qing Miao <cdutwhu@outlook.com>
2023-09-07 16:02:09 +1000

    * vite.config.ts add server

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,9 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    server: {
+        host: true,
+        port: 8080
     }
 })
